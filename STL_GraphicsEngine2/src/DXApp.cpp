ee7d560cc0fbcffaa6c2118ff892664267b6d2d0
mingyu0403 <jmg6017@naver.com>
2021-02-17 15:51:02 +0900

    DXApp 부모 클래스 추가.

---
+++ STL_GraphicsEngine2/src/DXApp.cpp
@@ -0,0 +1,19 @@
+#include "DXApp.h"
+
+DXApp::DXApp(HINSTANCE hInstance, int width, int height, std::wstring title)
+{
+}
+
+DXApp::~DXApp()
+{
+}
+
+bool DXApp::Initialize()
+{
+    return false;
+}
+
+bool DXApp::InitializeDirect3D()
+{
+    return false;
+}
