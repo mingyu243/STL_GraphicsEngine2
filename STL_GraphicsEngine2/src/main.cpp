92e5ba62ee3b115bb1b83d04f0680c1e472a22c5
mingyu0403 <jmg6017@naver.com>
2021-02-22 18:08:21 +0900

    Vector3f 클래스 추가
    제작 중.

--- STL_GraphicsEngine2/src/main.cpp
+++ STL_GraphicsEngine2/src/main.cpp
@@ -1,4 +1,7 @@
 #include "Engine.h"
+#include "public/Vector3f.h"
+
+#include <iostream>
 
 int WINAPI WinMain(
 	HINSTANCE hInstance,
@@ -6,16 +9,21 @@ int WINAPI WinMain(
 	LPSTR pCmdLine,
 	int nCmdShow)
 {
-	// 객체.
-	Engine engine = Engine(hInstance, 1280, 800, L"그래픽스 엔진");
+	//// 객체.
+	//Engine engine = Engine(hInstance, 1280, 800, L"그래픽스 엔진");
 
-	// 초기화.
-	if (engine.Initialize() == false)
-	{
-		MessageBox(nullptr, L"엔진 초기화 실패", L"오류", 0);
-		exit(-1);
-	}
-	
-	//  실행.
-	return engine.Run();
+	//// 초기화.
+	//if (engine.Initialize() == false)
+	//{
+	//	MessageBox(nullptr, L"엔진 초기화 실패", L"오류", 0);
+	//	exit(-1);
+	//}
+	//
+	////  실행.
+	//return engine.Run();
+
+	Vector3f vector(1, 1, 1);
+	Vector3f vector2(2, 2, 2);
+	Vector3f vector3 = vector + vector2;
+
 }
